a18a419923989f3060ac29379f8ea2408bb607fe
gadol21 <omerkattan@gmail.com>
2018-06-21 00:24:34 +0300

    CookieCommunicator can arm/disarm

--- app/src/main/java/com/example/fll_6/myapplication/MainActivity.java
+++ app/src/main/java/com/example/fll_6/myapplication/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.fll_6.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
